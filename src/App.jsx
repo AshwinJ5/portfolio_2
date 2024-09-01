7fe30926ac5280f5aec992ed409ce154a503a50a
AshwinJ5 <ashwinjoseph03@gmail.com>
2024-09-01 23:25:09 +0530

    portfolio_2

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,51 @@
-import './App.css'
-import About from './Components/About'
-import Contact from './Components/Contact'
-import Education from './Components/Education'
-import Footer from './Components/Footer'
-import Navbar from './Components/Navbar'
-import Projects from './Components/Projects'
-import Services from './Components/Services'
-import Skills from './Components/Skills'
-import 'aos/dist/aos.css'
+import React, { useEffect, useRef } from 'react';
+import './App.css';
+import About from './Components/About';
+import Contact from './Components/Contact';
+import Education from './Components/Education';
+import Footer from './Components/Footer';
+import Navbar from './Components/Navbar';
+import Projects from './Components/Projects';
+import Services from './Components/Services';
+import Skills from './Components/Skills';
+import 'aos/dist/aos.css';
 
 function App() {
-  const toTop=document.querySelector(".to-top")
-  window.addEventListener("scroll",()=>{
-    if(window.pageYOffset>100){
-      toTop.classList.add("active")
-    }else{
-      toTop.classList.remove("active")
+  const toTopRef = useRef(null);
 
-    }
-  })
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.pageYOffset > 100) {
+        toTopRef.current.classList.add('active');
+      } else {
+        toTopRef.current.classList.remove('active');
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
     <>
-    <section>
-      <Navbar/>
-      <About/>
-      <Skills/>
-      <Projects/>
-      <Education/>
-      <Services/>
-      <Contact/>
-      <Footer/>
-
-    </section>
-      <a href="#" className="to-top"><i class="fa-solid fa-chevron-up"></i></a>
+      <section>
+        <Navbar />
+        <About />
+        <Skills />
+        <Projects />
+        <Education />
+        <Services />
+        <Contact />
+        <Footer />
+      </section>
+      <a href="#" className="to-top" ref={toTopRef}>
+        <i className="fa-solid fa-chevron-up"></i>
+      </a>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
